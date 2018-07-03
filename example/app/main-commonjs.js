d1872cd433d7a1fc9ce2b3666c33dd9a978b1353
Vesa Laakso <laakso.vesa@gmail.com>
2018-07-03 13:48:02 +0300

    Run prettier fo rall files

--- example/app/main-commonjs.js
+++ example/app/main-commonjs.js
@@ -3,5 +3,5 @@ const I18n = require("i18n-js");
 window.I18n = I18n;
 I18n.translations = I18N_RUNTIME_TRANSLATIONS;
 
-require('./src/file1');
-require('./src/file2');
+require("./src/file1");
+require("./src/file2");
