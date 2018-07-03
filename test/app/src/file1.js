80169da8db05b5c333cd5551788c2836905b257d
Vesa Laakso <laakso.vesa@gmail.com>
2018-07-03 11:23:26 +0300

    Add first working version

--- test/app/src/file1.js
+++ test/app/src/file1.js
@@ -1,3 +1,1 @@
-const I18n = require("i18n-js");
-
 console.log(I18n.t('hello_world'));
--- test/webpack.config.js
+++ test/webpack.config.js
@@ -1,5 +1,8 @@
+/* @flow */
+
 const path = require("path");
 const nodeExternals = require("webpack-node-externals");
+const I18nRuntimePlugin = require("../lib/index");
 
 module.exports = {
   entry: {
@@ -8,12 +11,23 @@ module.exports = {
   output: {
     path: path.join(__dirname, "build")
   },
+  plugins: [
+    new I18nRuntimePlugin({
+      fullTranslations: {
+        en: { hello_world: "Hello, world!" },
+        fi: { hello_world: "Hei, maailma!" },
+        sv: { hello_world: "Och samma på svenska" }
+      },
+      functionNames: ["I18n.t"],
+      translationPlaceholderConstantName: "I18N_RUNTIME_TRANSLATIONS"
+    })
+  ],
   devtool: false,
   externals: [nodeExternals()],
   optimization: {
     runtimeChunk: {
-      name: 'runtimeChunk'
+      name: "runtimeChunk"
     }
   },
-  mode: 'development'
+  mode: "development"
 };
