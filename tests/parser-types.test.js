aaa1b0a1a3687178d357e030fda6c1ae380f76f6
Vesa Laakso <laakso.vesa@gmail.com>
2018-07-12 10:48:56 +0300

    Add test for ESM in entry point

--- tests/parser-types.test.js
+++ tests/parser-types.test.js
@@ -44,3 +44,41 @@ test("ESM with javascript/auto works", async () => {
     en: "Europe"
   });
 });
+
+test("with ESM in entry point works", async () => {
+  const pluginConfig = {
+    fullTranslations: {
+      en: { europe: "Europe" },
+      fi: { europe: "Eurooppa" }
+    },
+    functionNames: ["I18n.t"],
+    translationPlaceholderConstantName: "I18N_RUNTIME_TRANSLATIONS"
+  };
+
+  await compile({
+    plugin: new I18nRuntimePlugin(pluginConfig),
+
+    modules: {
+      "nonStrictEsm.js": `
+        export const en = I18n.t("europe", { locale: "en" });
+        export const fi = I18n.t("europe", { locale: "fi" });
+      `
+    },
+
+    entryCode: `
+      import I18n from "i18n-js";
+      import * as data from './nonStrictEsm';
+
+      window.I18n = I18n;
+      I18n.locale = 'en';
+      I18n.translations = I18N_RUNTIME_TRANSLATIONS;
+
+      window.testData = { fi: data.fi, en: data.en };
+    `
+  });
+
+  expect(window.testData).toEqual({
+    fi: "Eurooppa",
+    en: "Europe"
+  });
+});
